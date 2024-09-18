ac55925c97b3e78c4e41bfde19c80e0e6387fc04
tahomee <thaothanh6g@gmail.com>
2024-09-18 19:24:12 +0700

    Bai toan nong trai

--- obj/Debug/net8.0/BT1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BT1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2364513b8490ea86162ce882378a31d703598f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
